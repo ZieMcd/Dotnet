eaf0199e046b5e7bcbcf3b12890f4b5b84a61248
Zie Mcdowell <ziemcd@gmail.com>
2021-02-16 12:23:47 +0200

    adding a WebAPIClient

---
+++ AspDotNet/WebAPIClient/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace WebAPIClient
+{
+    class Program
+    {
+        private static readonly HttpClient client = new HttpClient();
+        static async Task Main(string[] args)
+        {
+            await ProcessRepositories();
+        }
+
+        private static async Task ProcessRepositories() 
+        {
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+            
+            var stringTask = client.GetStringAsync("https://jsonplaceholder.typicode.com/todos/1");
+            var msg = await stringTask;
+            System.Console.WriteLine(msg);
+        }
+    }
+}
